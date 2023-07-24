d35534b0bac6f692d8527d8962978466f0102281
Young Joon Lee <entelecheia@hotmail.com>
2023-07-24 18:26:28 +0900

    feat(lexikanon/utils/hanja): add new translation functionality

---
+++ src/lexikanon/utils/hanja/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""두음법칙에 관련된 내용은
+http://ko.wikipedia.org/wiki/%EB%91%90%EC%9D%8C_%EB%B2%95%EC%B9%99 를 참고.
+"""
+from .impl import translate
+
+__author__ = "Sumin Byeon"
+__email__ = "suminb@gmail.com"
+__version__ = "0.13.3"
