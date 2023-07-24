32be6ae7c37b5eaefb605c03f729cd1f0e098991
Young Joon Lee <entelecheia@hotmail.com>
2023-07-24 18:27:07 +0900

    feat(tests): add stopwords test in lexikanon module

---
+++ tests/lexikanon/stopwords/test_stopwords.py
@@ -0,0 +1,15 @@
+from lexikanon import HyFI
+from lexikanon.stopwords import Stopwords
+
+
+def test_stopwords():
+    print(HyFI.get_caller_module_name())
+    cfg = HyFI.compose_as_dict("stopwords")
+    cfg["nltk_stopwords_lang"] = "english"
+    stop = Stopwords(**cfg)
+    print(stop.stopwords_list)
+    assert len(stop.stopwords_list) == 179
+
+
+if __name__ == "__main__":
+    test_stopwords()
