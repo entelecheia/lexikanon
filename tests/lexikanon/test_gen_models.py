4a1845044cdfa60ef0af43f64d18f89fc0dd3379
Young Joon Lee <entelecheia@hotmail.com>
2023-08-13 12:10:09 +0900

    test: add Tokenizer to test_gen_models.py

--- tests/lexikanon/test_gen_models.py
+++ tests/lexikanon/test_gen_models.py
@@ -1,9 +1,10 @@
-from lexikanon.tokenizers import NLTKTagger, MecabTagger
+from lexikanon.tokenizers import NLTKTagger, MecabTagger, Tokenizer
 
 
 def test_gen_model_configs():
     print(NLTKTagger.generate_config())
     print(MecabTagger.generate_config())
+    print(Tokenizer.generate_config())
 
 
 if __name__ == "__main__":
